eecd34407899bce7fa9ae6e27b0a3ed06bd24890
chuck21shin <sjcshs@gmail.com>
2017-03-26 17:14:47 +0900

    gg

--- server.js
+++ server.js
@@ -27,14 +27,15 @@ var server = http.createServer(app);
 server.listen(port);
 
 //Notificationhub setup
-var hubName = 'test';
-var connectionString = 'Endpoint=sb://azurenotificationtest.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=Q+Rgz8F1XhNO83Tl3GMytYa3jE6PGS6zAy1WliFD2zg=';
+var hubName = 'tt';
+var connectionString = 'Endpoint=sb://tettt555.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=rTveSY3YUfovGjLjkSl5e5DxJEXCulyBWH5k6RoiaTk=';
 var notificationHubService = azure.createNotificationHubService(hubName,connectionString);
 
 
 var io = require('socket.io').listen(server);
 io.sockets.on('connection',function(socket){
     socket.emit('toclient',{msg:'Welcome!'});
+    //xnull으 트겅이마가ㅡㅇ
     notificationHubService.gcm.send(null, {data:{id:socket.id, message:'Welcome'}},function(error){
         if(!error){
             console.log('send');
@@ -46,6 +47,7 @@ io.sockets.on('connection',function(socket){
         socket.emit('toclient',data); // 해당 클라이언트에게만 보냄. 다른 클라이언트에 보낼려면?
         console.log('Message from client :'+data.msg);
        
+       //burg 수엉
         if(!data.msg==""){
             notificationHubService.gcm.send(null, {data:{id:socket.id, message:data.msg}}, function(error){
                 if(!error){
